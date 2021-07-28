d4d2a98e47fb1c9675f512c4ab23750b829d6c8d
Patryk Orłowski <orzeleagle122@gmail.com>
2021-07-28 12:53:28 +0200

    add styling error content

---
+++ src/components/molecules/ErrorMessge/ErrorMessge.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title } from '../../atoms/Title/Title';
+import { Wrapper } from './ErrorMessge.styles';
+
+const ErrorMessge = () => {
+  return (
+    <Wrapper>
+      <Title> Title </Title>
+      <p>Lorem ipsum dolor sit amet</p>
+    </Wrapper>
+  );
+};
+
+export default ErrorMessge;
